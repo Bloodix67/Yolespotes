6fea554468eea1a137b0ea97abb99fcb683b63ee
emma <activ_admin@ACTIV_ADMIN-PC>
2016-11-22 15:08:44 +0100

    Initialisation

---
+++ YoLesPotesConsole/YoLesPoteConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoLesPoteConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salut à tous");
+            Console.Beep();
+            Console.ReadKey();
+        }
+    }
+}
