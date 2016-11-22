c76f7d0184aab2363cb29bdbc778fc59c7fb43af
emma <activ_admin@ACTIV_ADMIN-PC>
2016-11-22 15:33:42 +0100

    Super mélodie

--- YoLesPotesConsole/YoLesPoteConsole/Program.cs
+++ YoLesPotesConsole/YoLesPoteConsole/Program.cs
@@ -11,8 +11,40 @@ namespace YoLesPoteConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Salut à tous");
-            Console.Beep();
-            Console.ReadKey();
+            Console.Beep(523, 1000);
+            Console.Beep(580, 1000);
+            Console.Beep(650, 1000);
+            Console.Beep(523, 1000);
+            Console.Beep(523, 1000);
+            Console.Beep(580, 1000);
+            Console.Beep(650, 1000);
+            Console.Beep(523, 1000);
+            Console.Beep(650, 1000);
+            Console.Beep(680, 1000);
+            Console.Beep(760, 1500);
+            Console.Beep(650, 1000);
+            Console.Beep(680, 1000);
+            Console.Beep(760, 1500);
+            Console.Beep(760, 750);
+            Console.Beep(840, 750);
+            Console.Beep(760, 750);
+            Console.Beep(680, 750);
+            Console.Beep(650, 1500);
+            Console.Beep(523, 1500);
+            Console.Beep(760, 750);
+            Console.Beep(840, 750);
+            Console.Beep(760, 750);
+            Console.Beep(680, 750);
+            Console.Beep(650, 1500);
+            Console.Beep(523, 1500);
+            Console.Beep(523, 1500);
+            Console.Beep(400, 1500);
+            Console.Beep(523, 1500);
+            Console.Beep(523, 1500);
+            Console.Beep(400, 1500);
+            Console.Beep(523, 1500);
+
+
         }
     }
 }
